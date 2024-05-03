b04b2d4c480386ba80a3bb8f08d052427ff55998
Víctor Morilla <Test@ecb.com>
2024-05-03 14:40:38 +0200

    Removed yargs dependencies

--- packages/csv2xbrl-cli/src/index.ts
+++ packages/csv2xbrl-cli/src/index.ts
@@ -2,32 +2,31 @@
 import { csv2xbrl, loadPackages } from "csv2xbrl";
 import { program } from '@commander-js/extra-typings';
 import fs from "fs";
+import { exit } from "process";
 
 function parseArgs() {
     return program
         .name("csv2xbrl")
         .description("JSON files to convert to xBRL")
-        .argument("<jsonFiles...>", "JSON files to convert to xBRL")
-        .option("-p, --package <package>", "Taxonomy package zip files to load")
+        .option("-p, --packages <package...>", "Taxonomy package zip files to load")
         .option("-o, --output <output>", "Output file. Standard output if - is specified. If not specified, "
             + "it uses the same name of the input file, but with the .xbrl extension.")
-        .action((args, options, logger) => {
-            if (args.length > 1 && options.output) {
+        .requiredOption("-j, --json <json...>", "JSON files to convert to xBRL")
+        .action((options, logger) => {
+            if (options.json.length > 1 && options.output) {
                 logger.error("Output file can only be specified when converting a single JSON file.");
                 process.exit(1);
             }
-        })
-        .parse();
+        });
 }
 
 
 async function main() {
     const command = parseArgs();
-    const args = command.args;
     const options = command.opts();
 
-    const resolver = await loadPackages(options.package?.split(",") as string[]);
-    for (const jsonFile of args) {
+    const resolver = await loadPackages(options.packages || []);
+    for (const jsonFile of options.json) {
         try {
             const outputFile = options.output || jsonFile.replace(/\.json$/, ".xbrl");
             const output = outputFile === "-" ? process.stdout : fs.createWriteStream(outputFile);
